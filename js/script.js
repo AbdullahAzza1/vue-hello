45047dc740875a8af1aee0ddff715ad217d76276
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-15 16:53:16 +0100

    step2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
+const { createApp } = vue;
 
 createApp({
-  data() {
-    return {
-      message: 'ciao user'
+    data() {
+        return {
+            message: 'ciao user'
+            numeretto: '100'
+        }
     }
-  }
 }).mount('#app')
